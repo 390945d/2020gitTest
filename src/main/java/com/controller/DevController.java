d811c1caf4fb510139131177ac2a6f26768f8919
390945dev <xiangcll@foxmail.com>
2020-03-14 15:01:41 +0800

    dev 1

--- src/main/java/com/controller/DevController.java
+++ src/main/java/com/controller/DevController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DevController {
-    @RequestMapping("/adkkev")
+    @RequestMapping("/getarrr")
     public String adddev(){
-        return "u8uuh";
+        return "1";
     }
 
     @RequestMapping("/send")
