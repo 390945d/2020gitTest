e4cab409937aba07b0352844fd40156114b4e18c
xcl <913047924@qq.com>
2020-03-14 15:36:19 +0800

    dev 5

--- src/main/java/com/controller/DevController.java
+++ src/main/java/com/controller/DevController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DevController {
-    @RequestMapping("/get333")
+    @RequestMapping("/getdev11111")
     public String adddev(){
         return "3636";
     }
 
-    @RequestMapping("/send4444")
+    @RequestMapping("/send2222222")
     public String send(){
         return "uuuu";
     }
