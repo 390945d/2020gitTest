b17b35fee462241ff19ee72914a682fe346e54c6
xcl <913047924@qq.com>
2020-03-14 09:09:38 +0800

    first

---
+++ src/main/java/com/controller/SendController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SendController {
+    @RequestMapping("/get")
+    public String get(){
+        System.out.println("hihihi");
+        return "jiji";
+    }
+}
