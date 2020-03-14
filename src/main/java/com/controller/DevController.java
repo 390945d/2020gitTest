bb444450a4a29e1e19632d8916887077ed375a90
xcl <913047924@qq.com>
2020-03-14 11:44:05 +0800

    dev提交分支

--- src/main/java/com/controller/DevController.java
+++ src/main/java/com/controller/DevController.java
@@ -4,16 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DevController {
-    @RequestMapping("/jiji")
-    public String hihi(){
-        return "";
-    }
-    @RequestMapping("/add")
-    public String add(){
-        return "";
-    }
-    @RequestMapping("/add3")
-    public String add3(){
+    @RequestMapping("/adddev")
+    public String adddev(){
         return "";
     }
 }
