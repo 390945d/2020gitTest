5829ce302af0effeeb6e47373b7c7f5c37d6e303
xcl <913047924@qq.com>
2020-03-14 14:47:50 +0800

    new dev

--- src/main/java/com/controller/DevController.java
+++ src/main/java/com/controller/DevController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DevController {
-    @RequestMapping("/adddev")
+    @RequestMapping("/adkkev")
     public String adddev(){
-        return "";
+        return "u8uuh";
     }
 }
