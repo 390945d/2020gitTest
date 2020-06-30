05befde0d74df1c23334a16b0a3f26530d8ef4e1
390945 <913047924@qq.com>
2020-06-30 18:16:08 +0800

    hhh

--- src/main/java/com/controller/AddDevController.java
+++ src/main/java/com/controller/AddDevController.java
@@ -1,4 +1,10 @@
 package com.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class AddDevController {
+    @RequestMapping("/getdev11111")
+    public String adddev(){
+        return "3636";
+    }
 }
